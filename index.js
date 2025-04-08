9721a6d1248ac6785c50779e00a4a66af438cd66
José Isaac <joseisaacnascimento@gmail.com>
2025-04-08 13:41:08 -0300

    feat: Adiciona checkbox para marcar a tarefa como concluída

--- index.js
+++ index.js
@@ -13,22 +13,50 @@ formElement.addEventListener("submit", (event) => {
 
   // verifica se o input está vazio
   if (inputElement.value === "") {
+    // mensagem de erro
     let msgError = document.querySelector("#msg-error");
+    // adiciona a classe para ela aparecer sua animação
+    msgError.classList.add("aparece");
+    //    altera o conteúdo da mensagem
     msgError.textContent = "Por favor, insira uma tarefa!";
   } else {
     //   cria um elemento li
     let liElement = document.createElement("li");
 
+    // elemento checkbox
+    let checkboxElement = document.createElement("input");
+    checkboxElement.type = "checkbox";
+
+    // adiciona o checkbox ao li
+    liElement.appendChild(checkboxElement);
+
     // valor do input
     let valorInput = inputElement.value;
 
-    // adiciona o valor do input ao li
-    liElement.textContent = valorInput;
+    // adiciona o checkbox e valor do input ao li
+    liElement.innerHTML = `${checkboxElement.outerHTML} ${valorInput}`;
 
     // adiciona o li ao ul
     ulElement.appendChild(liElement);
 
     // limpa o input
     inputElement.value = "";
+
+    // mensagem de adicionado
+    let msgError = document.querySelector("#msg-error");
+    // adiciona a classe para ela aparecer sua animação
+    msgError.classList.add("aparece");
+    //    altera o conteúdo da mensagem
+    msgError.textContent = "Tarefa adicionada!";
+    msgError.style.color = "#05f0f8";
+  }
+});
+
+// evento de click no checkbox
+ulElement.addEventListener("click", (event) => {
+  // verifica se o elemento clicado é um checkbox
+  if (event.target.type === "checkbox") {
+    // remove o li
+    event.target.parentElement.remove();
   }
 });
